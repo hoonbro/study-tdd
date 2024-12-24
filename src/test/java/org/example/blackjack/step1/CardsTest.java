f8d296f8b5598780f41da729223348e89c87cc7c
= <wlgns1588@naver.com>
2024-12-24 17:22:50 +0900

    CARD 클래스 수정 및 테스트 코드 추가중

---
+++ src/test/java/org/example/blackjack/step1/CardsTest.java
@@ -0,0 +1,32 @@
+package org.example.blackjack.step1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CardsTest {
+
+    private Cards cards ;
+    @BeforeEach
+    void setup(){
+        cards = new Cards();
+
+        cards.addCard(new Card(CardShape.SPADES, CardValue.ACE));
+        cards.addCard(new Card(CardShape.HEARTS, CardValue.SEVEN));
+        cards.addCard(new Card(CardShape.CLUBS, CardValue.THREE));
+    }
+
+    @Test
+    void getScore() {
+        assertThat(cards.getScore()).isEqualTo(11);
+    }
+
+    @Test
+    void addCard() {
+        int beforeSize = cards.getSize();
+        cards.addCard(new Card(CardShape.DIAMONDS, CardValue.QUEEN));
+
+        assertThat(beforeSize).isEqualTo(cards.getSize()-1);
+    }
+}
